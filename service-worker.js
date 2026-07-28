a0c1b97ac8a908f9b831e9f60f58b8223c3fbbe9
Costieman <65705627+Costieman@users.noreply.github.com>
2026-07-28 15:24:01 +0800

    Refresh GitHub Pages cache after login fix

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "salita-quest-v5-4-full-screen-profiles-r3";
+const CACHE_NAME = "salita-quest-v5-4-full-screen-profiles-r4";
 const STATIC_FILES = [
-  "./", "./index.html", "./app.html", "./profile-shell.css", "./profile-shell.js", "./profile-app.js",
+  "./", "./index.html", "./app.html", "./profile-shell.css", "./profile-app.js",
   "./style.css", "./app.js", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png",
   "./avatars/tarsier.png", "./avatars/eagle.png", "./avatars/tamaraw.png", "./avatars/peacock.png",
   "./avatars/orchid.png", "./avatars/jade.png", "./avatars/rafflesia.png", "./avatars/anahaw.png"
